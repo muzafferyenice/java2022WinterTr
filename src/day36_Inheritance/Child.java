7a1fec5073a9beb17a92dc22fbf7c6d749dba971
MuzafferYenice <muzafferyenicet@gmail.com>
2022-04-07 11:45:21 +0300

    07/04/2022

--- src/day36_Inheritance/Child.java
+++ src/day36_Inheritance/Child.java
@@ -6,7 +6,8 @@ public class Child extends Parent{
         System.out.println("Child Cons Calisti");
     }
         /*tum class larda biz gormesek bile Java nin olusturdugu default
-        constructor vardir extends keyword kullanan class lardaki tum
+        constructor vardir
+        extends keyword kullanan class lardaki tum
         CONSTRUCTOR larin ilk satirinda biz gormesek bile super();
         cons call vardir yani parent class in parametresiz constructor call
 
--- src/day36_Inheritance/Parent.java
+++ src/day36_Inheritance/Parent.java
@@ -1,7 +1,10 @@
 package day36_Inheritance;
 
 public class Parent {
-    public Parent() {
+    protected Parent() {
         System.out.println("Parent Cons Calisti");
+    }
+    public Parent (int sayi3,int sayi4){
+        System.out.println("parent 2 pm li con");
     }
 }
