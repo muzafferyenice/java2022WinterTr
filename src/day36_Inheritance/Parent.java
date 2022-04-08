a1b01d05e2c6ccd771a52d29d0d231a7d38a6141
MuzafferYenice <muzafferyenicet@gmail.com>
2022-04-08 15:54:29 +0300

    08/04/2022

--- src/day36_Inheritance/Parent.java
+++ src/day36_Inheritance/Parent.java
@@ -4,7 +4,11 @@ public class Parent {
     protected Parent() {
         System.out.println("Parent Cons Calisti");
     }
-    public Parent (int sayi3,int sayi4){
+
+    public Parent(int sayi3, int sayi4) {
         System.out.println("parent 2 pm li con");
     }
+
+
 }
+
