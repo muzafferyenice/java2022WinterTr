64202e1d8329beaa93be9135b99c07da8d26f485
MuzafferYenice <muzafferyenicet@gmail.com>
2022-04-23 02:30:52 +0300

    22/04/2022

--- src/day49_maps/MapOlustur.java
+++ src/day49_maps/MapOlustur.java
@@ -7,9 +7,11 @@ public class MapOlustur {
     public static Map<Integer,String> myMap(){
         Map<Integer,String> sinifList=new HashMap<>();
 
-        sinifList.put(101,"Ali, Can, Dev");
-        sinifList.put(102,"VAli, Can, Qa");
-        sinifList.put(103,"Veli, Yan, Dev");
+        sinifList.put(101,"Ali, Can, Dev, 123");
+        sinifList.put(102,"VAli, Can, Qa, 124");
+        sinifList.put(103,"Veli, Yan, Dev, 111");
+        sinifList.put(104,"eli, an, Java, 1232");
+        sinifList.put(105,"deli, Yn, Java, 1212");
 
         return sinifList;
     }
